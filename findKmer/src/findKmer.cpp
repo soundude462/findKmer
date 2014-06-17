ff5d9f9ac2fa3c31a0a8799fe88a1674978a5b83
Kalen Brown <soundude462@gmail.com>
2014-06-17 09:25:59 -0500

    5

--- findKmer/src/findKmer.cpp
+++ findKmer/src/findKmer.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Git Hub Push3!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Git Hub Push5!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
