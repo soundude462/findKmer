4e6eeb952a4c91e7c4e08c2c4afdde650aab7d75
Kalen Brown <soundude462@gmail.com>
2014-06-17 09:20:39 -0500

    First Push

--- findKmer/src/findKmer.cpp
+++ findKmer/src/findKmer.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Git Hub Two!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Git Hub Push!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
