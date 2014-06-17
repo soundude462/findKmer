035fdc42297d8c72d7bb544f8e63c20b86568f22
Kalen Brown <soundude462@gmail.com>
2014-06-17 11:36:09 -0500

    Adding file reading operations. Cannot open the file for some reason.

--- findKmer/src/findKmer.cpp
+++ findKmer/src/findKmer.cpp
@@ -7,9 +7,60 @@
 //============================================================================
 
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <string.h> //for strcmp(string1,string2) string comparison returns a 0 if they are the same.
+#include <stdlib.h> //malloc is in this.
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello Git Hub Push6!!!" << endl; // prints !!!Hello World!!!
+// Data structure for a tree.
+struct node {
+	char base;
+	struct node *nextBasePtr[4];
+	unsigned int counter;
+};
+
+int char2int(char base);
+
+int main(int argc, char *argv[]) {
+	FILE *fp = NULL;
+	char fileName[] = "homo_sapiensupstream.fas";
+	char buffer[1000]; //todo commandline arg
+	char base;
+	cout << "!!!Find The KMER!!!" << endl; // prints !!!Hello World!!!
+
+
+	if ((fp = fopen("text.txt", "r")) == NULL) {
+		printf("File could not be opened. Ending program.\n");
+		return (-1);
+	}
+
+	char2int(base);
+
+
+	fclose(fp);
+	printf("End of program\n");
 	return 0;
+}
+
+int char2int(char base) {
+	int integer = -1;
+	if (base == 'A') {
+		integer = 0;
+	} else if (base == 'C') {
+		integer = 1;
+	} else if (base == 'G') {
+		integer = 2;
+	} else if (base == 'T') {
+		integer = 3;
+	} else if (base == 'N') {
+		integer = -2;
+	} else if (base == '\n') {
+		integer = -3;
+	}
+
+//close the function
+	return integer;
 }
