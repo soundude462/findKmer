70ebc1236476c454277fa42e20396806792d2012
Kalen Brown <soundude462@gmail.com>
2014-06-17 09:14:38 -0500

    Verify

--- findKmer/src/findKmer.cpp
+++ findKmer/src/findKmer.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Git Hub!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Git Hub Two!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
