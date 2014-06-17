44e23b5ad7bbdef96b4674b526ab7b6dd00ea8ca
Kalen Brown <soundude462@gmail.com>
2014-06-17 09:13:17 -0500

    initial commit

---
+++ findKmer/src/findKmer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : findKmer.cpp
+// Author      : Kalen Brown and Gus Thies
+// Version     :
+// Copyright   : Do not copy
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Git Hub!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
