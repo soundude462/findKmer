ca3ffe0b30fd85bc88b4e5c7196223f1b1129f6a
Kalen Brown <soundude462@gmail.com>
2014-08-27 16:46:33 -0500

    Fixed copyright errors.

--- findKmer/src/findKmer.cpp
+++ findKmer/src/findKmer.cpp
@@ -1,7 +1,8 @@
 /*
- * Copyright (c) 2014 Kalen A. Brown, August C. Thies, Gavin Conant, Xiang Wang, and Michela Becchi and University of Missouri in Columbia.
+ * Copyright (c) 2014 Kalen A. Brown, August C. Thies, Gavin Conant, Xiang Wang,
+ * Michela Becchi and University of Missouri in Columbia.
  * All rights reserved
- *s
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
@@ -10,18 +11,20 @@
  *    2. Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *    3. The name of the author or Washington University may not be used
+ *    3. The name of the author or the University may not be used
  *       to endorse or promote products derived from this source code
  *       without specific prior written permission.
  *    4. Conditions of any other entities that contributed to this are also
  *       met. If a copyright notice is present from another entity, it must
  *       be maintained in redistributions of the source code.
+ *    5. You notify the author and give your intentions.
+ *       Notification can be given to kab8c8 at mail dot missouri dot edu
  *
  * THIS INTELLECTUAL PROPERTY (WHICH MAY INCLUDE BUT IS NOT LIMITED TO SOFTWARE,
- * FIRMWARE, VHDL, etc) IS PROVIDED BY  THE AUTHOR AND WASHINGTON UNIVERSITY
+ * FIRMWARE, VHDL, etc) IS PROVIDED BY  THE AUTHOR AND THE UNIVERSITY
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR WASHINGTON UNIVERSITY
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR THE UNIVERSITY
  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
